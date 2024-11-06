95988518d3da42cae79e9534093279ca2a926848
unknown <shshumay@gmail.com>
2024-11-06 22:07:19 +0700

    tambah page login + register

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.auth_layout')
+@section('content')
+<div class="container">
+
+    <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+            <div class="card mb-3">
+
+              <div class="card-body">
+
+                <div class="pt-4 pb-2">
+                  <h5 class="card-title text-center pb-0 fs-4">Register Your Account</h5>
+                  <p class="text-center small">Enter your personal details to create account</p>
+                </div>
+
+                <form class="row g-3" method="POST" action="">
+                  @csrf
+                  <div class="col-12">
+                    <label for="yourNama" class="form-label">Nama</label>
+                    <div class="input-group has-validation">
+                      <input type="text" name="Nama" class="form-control @error('Nama') is-invalid @enderror"  value="{{ old('Nama') }}" id="yourNama" required>
+                      @error('Nama')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                      @enderror
+                    </div>
+                  </div>
+
+                  <div class="col-12">
+                    <label for="yourUsername" class="form-label">Username</label>
+                    <div class="input-group has-validation">
+                      <input type="text" name="username" class="form-control @error('username') is-invalid @enderror"  value="{{ old('username') }}" id="yourUsername" required>
+                      @error('username')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                      @enderror
+                    </div>
+                  </div>
+
+                  <div class="col-12">
+                    <label for="yourPassword" class="form-label">Password</label>
+                    <input type="password" name="password" class="form-control" id="yourPassword" required>
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                  </div>
+
+                  <div class="col-12">
+                    <label for="yourPasswordConfirm" class="form-label">Konfirmasi Password</label>
+                    <input type="password" name="password_confirmation" class="form-control" id="yourPasswordConfirm" required>
+                  </div>
+
+                  <div class="col-12 mt-5">
+                    <button class="btn btn-primary w-100" type="submit">Register</button>
+                  </div>
+                  <div class="col-12">
+                    <p class="small mb-0">Already have account? <a href="">Log in</a></p>
+                  </div>
+                </form>
+
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+
+    </section>
+
+  </div>
+@endsection
---
+++ resources/views/layouts/auth_layout.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Login</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="{{asset('assets\img\tirta_wibawa_mukti_logo.png')}}" rel="icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+<!-- Vendor CSS Files -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <style>
+    body {
+        background: url("{{asset('assets/img/header.png')}}") no-repeat; /* Hindari pengulangan gambar */
+        background-size: 100% 100%; /* Menyesuaikan gambar dengan ukuran halaman */
+        background-position: center; /* Posisikan gambar di tengah halaman */
+    }
+  </style>
+</head>
+
+<body>
+
+  <main>
+    @yield('content')
+  </main><!-- End #main -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" ></script>
+<script src="https://cdn.jsdelivr.net/gh/your-username/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="https://yourwebsite.com/assets/js/main.js"></script>
+
+  {{-- <script src="assets/vendor/sweet-alert/sweetalert2@11.js"></script> --}}
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+
+
+  --}}
+
+
+  @if(Session::has('failedlogin'))
+  <script>
+    Swal.fire('{{ Session::get('failedlogin') }}');
+  </script>
+@elseif(Session::has('successregis'))
+  <script>
+    Swal.fire('{{ Session::get('successregis') }}');
+  </script>
+@elseif(Session::has('successlogout'))
+<script>
+  Swal.fire('{{ Session::get('successlogout') }}');
+</script>
+@endif
+</body>
+
+</html>
