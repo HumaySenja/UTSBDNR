d1b211fcfd987fefa15cf43d1450f7061e994ed1
FRQNC <alhaqqi123@gmail.com>
2024-11-07 02:53:40 +0700

    fix after merging

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ Route::get('/shop', [ViewController::class, 'shopView'])->name('shop');
 
 Route::get('/shop-detail', [ViewController::class, 'shopDetailView'])->name('shop-detail');
 
-Route::get('/cart', [CartController::class, 'cartView'])->name('cart');
-
 Route::get('/checkout', [ViewController::class,'checkoutView'])->name('checkout');
 
 Route::get('/register', [UserController::class, 'registerView'])->name('register');
@@ -23,15 +21,14 @@ Route::get('/login', [UserController::class, 'loginView'])->name('login');
 Route::post('/login/process', [UserController::class, 'login']);
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/logout', [UserController::class, 'logout']);
+    Route::get('/logout', [UserController::class, 'logout'])->name('logout');
     Route::get('/history', [TransactionController::class, 'index']);
+    Route::get('/cart', [CartController::class, 'cartView'])->name('cart');
+    Route::post('/add-to-cart/{productId}', [CartController::class, 'addToCart'])->name('cart.add')->middleware('auth');
+    Route::delete('/cart/{cartId}', [CartController::class, 'removeFromCart'])->name('cart.remove');
+    Route::post('/cart/update-quantity', [CartController::class, 'updateQuantity'])->name('cart.updateQuantity');
 });
 
-Route::post('/add-to-cart/{productId}', [CartController::class, 'addToCart'])->name('cart.add')->middleware('auth');
-Route::get('/cart', [CartController::class, 'viewCart'])->name('cart');
-Route::delete('/cart/{cartId}', [CartController::class, 'removeFromCart'])->name('cart.remove');
-
-Route::post('/cart/update-quantity', [CartController::class, 'updateQuantity'])->name('cart.updateQuantity');
 
 
 
