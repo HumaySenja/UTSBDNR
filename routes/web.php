559deaf1f5514a57b93a9e7f53bc34b91dfe3120
Nabil Hanif <nabiltitimplik@gmail.com>
2024-11-06 22:09:40 +0700

    frontend

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/shop', function(){
+    return view('shop');
+})->name('shop');
+
+Route::get('/shop-detail', function(){
+    return view('shop-detail');
+})->name('shop-detail');
+
+Route::get('/cart', function(){
+    return view('cart');
+})->name('cart');
+
+Route::get('/checkout', function(){
+    return view('checkout');
+})->name('checkout');
+
